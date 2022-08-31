3ac31edf117dadbc2d9daf6df677850a3a81a099
nbhavna <niraj.bhavnani@morningstar.com>
2022-08-31 21:48:54 +0530

    Login implementation

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,28 +3,38 @@
     <h2>Login</h2>
 
     <label for="email">Email:</label>
-    <input type="email" name="email" v-model="email" required>
+    <input type="email" name="email" v-model="email" required />
 
     <label for="password">Password:</label>
-    <input type="password" name="password" v-model="password" required>
+    <input type="password" name="password" v-model="password" required />
 
-    <button>Login</button>
+    <button v-if="!isPending">Login</button>
+    <button v-if="isPending" disabled>Logging in</button>
+    <div v-if="error">{{ error }}</div>
   </form>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import useLogin from "../composables/useLogin";
 
 export default {
   setup() {
-    const email = ref('')
-    const password = ref('')
+    const email = ref("");
+    const password = ref("");
+
+    const { login, error, isPending } = useLogin();
+    const router = useRouter();
 
     const handleSubmit = async () => {
-      console.log(email.value, password.value)
-    }
+      await login(email.value, password.value);
+      if (!error.value) {
+        router.push("/");
+      }
+    };
 
-    return { email, password, handleSubmit }
-  }
-}
+    return { email, password, handleSubmit, error, isPending };
+  },
+};
 </script>
