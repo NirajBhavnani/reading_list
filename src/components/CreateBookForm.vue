5511d2c22b22c4712321ef9d6ee526f9bc6f4c5d
nbhavna <niraj.bhavnani@morningstar.com>
2022-08-31 21:04:30 +0530

    Adding new docs in firestore

--- src/components/CreateBookForm.vue
+++ src/components/CreateBookForm.vue
@@ -3,30 +3,41 @@
     <h3>Add a New Book</h3>
 
     <label for="title">Book title:</label>
-    <input type="text" name="title" v-model="title" required>
+    <input type="text" name="title" v-model="title" required />
 
     <label for="author">Book author:</label>
-    <input type="text" name="author" v-model="author" required>
+    <input type="text" name="author" v-model="author" required />
 
     <button>Add Book</button>
   </form>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref } from "vue";
+import { firestore } from "../firebase/config";
+import { collection, addDoc } from "firebase/firestore";
 
 export default {
   setup() {
-    const title = ref('')
-    const author = ref('')
+    const title = ref("");
+    const author = ref("");
 
     const handleSubmit = async () => {
-      console.log(title.value, author.value)
-    }
+      const colRef = collection(firestore, "books");
+      await addDoc(colRef, {
+        title: title.value,
+        author: author.value,
+        isFav: false,
+      });
 
-    return { handleSubmit, title, author }
-  }
-}
+      // reset the form
+      title.value = "";
+      author.value = "";
+    };
+
+    return { handleSubmit, title, author };
+  },
+};
 </script>
 
 <style>
