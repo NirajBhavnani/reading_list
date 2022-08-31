0b3203509c1e72bd3c68404d051a17ba588f72cb
nbhavna <niraj.bhavnani@morningstar.com>
2022-08-31 21:42:21 +0530

    Logout implementation

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,9 +6,9 @@
       <!-- for logged in users -->
       <div>
         <router-link to="/">Home</router-link>
-        <button>Logout</button>
+        <button @click="handleLogout">Logout</button>
       </div>
-      
+
       <!-- for logged out users -->
       <div>
         <router-link to="/login">Login</router-link>
@@ -19,9 +19,19 @@
 </template>
 
 <script>
-export default {
+// firebase imports
+import { auth } from "../firebase/config";
+import { signOut } from "firebase/auth";
 
-}
+export default {
+  setup() {
+    const handleLogout = () => {
+      signOut(auth);
+    };
+
+    return { handleLogout };
+  },
+};
 </script>
 
 <style>
